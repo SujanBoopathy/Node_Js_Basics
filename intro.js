930ddfb746a1c1d1aab92d8090320e35709619b4
sujan.ab <sujansaradha@gmail.com>
2024-04-28 14:33:00 +0530

    print the number in intro.js

--- intro.js
+++ intro.js
@@ -1,8 +1,8 @@
 const number = 10;
 //find number is odd or even in js
 if(number % 2 === 0){
-    console.log("it is even number");
+    console.log("it is even number",number);
 }
 else{
-    console.log("it is odd number");
+    console.log("it is odd number",number);
 }
